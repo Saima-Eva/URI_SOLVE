a060b9b7826da0b7e87f98db89f0a89cbb942e94
Saima Eva <69007708+Saima-Eva@users.noreply.github.com>
2021-01-03 00:54:40 +0600

    Add files via upload

---
+++ ExtremelyBasic_1001.java
@@ -0,0 +1,18 @@
+
+package URI_Online_Judge;
+
+import java.io.IOException;
+ import java.util.Scanner;
+public class ExtremelyBasic_1001 {
+      public static void main(String[] args) throws IOException {
+ 
+        Scanner input=new Scanner(System.in);
+    int A,B,C;
+    A=input.nextInt();
+    B=input.nextInt();
+    C=A+B;
+        System.out.println("X = "+C);
+ 
+    }
+    
+}

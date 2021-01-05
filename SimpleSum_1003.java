5176dd36571cdb65a93902fc89861f0d81cabd5f
Saima Eva <69007708+Saima-Eva@users.noreply.github.com>
2021-01-05 19:20:39 +0600

    Add files via upload

---
+++ SimpleSum_1003.java
@@ -0,0 +1,16 @@
+
+package URI_Online_Judge;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class SimpleSum_1003 {
+    public static void main(String[] args) throws IOException {
+        Scanner input=new Scanner(System.in);
+        int A,B,C;
+        A=input.nextInt();
+        B=input.nextInt();
+        C=A+B;
+        System.out.println("SOMA ="+C);
+        
+    }
+}

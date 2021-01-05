b60f2c8e40584b337107628688c8fae36bf8da8c
Saima Eva <69007708+Saima-Eva@users.noreply.github.com>
2021-01-05 19:40:20 +0600

    Add files via upload

---
+++ SimpleProduct_1004.java
@@ -0,0 +1,17 @@
+
+package URI_Online_Judge;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class SimpleProduct_1004 {
+    public static void main(String[] args)throws IOException {
+        Scanner key=new Scanner(System.in);
+        int A,B,C;
+        A=key.nextInt();
+        B=key.nextInt();
+        C=A*B;
+        System.out.println("PROD = "+C);
+        
+    }
+    
+}

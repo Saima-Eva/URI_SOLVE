898f643a00fec4501c09b327d7db6d3127c6bc79
Saima Eva <69007708+Saima-Eva@users.noreply.github.com>
2021-01-27 22:33:12 +0600

    Add files via upload

---
+++ Consumption_URI_1014.java
@@ -0,0 +1,23 @@
+
+package URI_Online_Judge;
+
+import java.io.IOException;
+ 
+import java.util.Scanner;
+public class Consumption_URI_1014 {
+     
+    public static void main(String[] args) throws IOException {
+ 
+      Scanner key=new Scanner(System.in);
+        
+        int x=key.nextInt();
+       
+       double y=key.nextDouble();
+        double z=x/y;
+        
+        
+        System.out.printf("%.3f km/l\n",z);
+        
+ 
+    }
+}
